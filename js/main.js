07353861b4cda01da1424c35af0c71c76c5ec875
webdev-alexey <alexey.anasov@yandex.by>
2023-09-19 20:52:04 +0300

    верстка + стилизация секции популярное, отель, активная

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 customSelect('select');
+
+ const picker = new easepick.create({
+		element: document.getElementById('date'),
+		css: [
+			'https://cdn.jsdelivr.net/npm/@easepick/bundle@1.2.1/dist/index.css',
+		],
+		format: 'DD.MM.YY',
+ });
